f0d06ac2fe2cdac6cdbe0474924cc0606a9dd1b0
djkram <marc.planaguma@gmail.com>
2016-02-07 20:22:49 +0100

    First Commit

---
+++ src/main/java/com/example/DemoIntApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.config.EnableIntegration;
+
+@SpringBootApplication
+@EnableIntegration
+@ImportResource({"classpath*:**/twitter-integration-connection.xml"})
+
+public class DemoIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIntApplication.class, args);
+	}
+}
